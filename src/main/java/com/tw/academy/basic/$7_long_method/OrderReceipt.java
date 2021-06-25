179081482360cdae547fdc8973a336a66eb01686
OOCLDM\HUANGHA4 <huangha4@oocl.com>
2021-06-25 20:40:51 +0800

    [$7, Refactor, orderReceipt] Extract calculate total method

--- src/main/java/com/tw/academy/basic/$7_long_method/OrderReceipt.java
+++ src/main/java/com/tw/academy/basic/$7_long_method/OrderReceipt.java
@@ -1,5 +1,7 @@
 package com.tw.academy.basic.$7_long_method;
 
+import java.util.List;
+
 public class OrderReceipt {
     private Order order;
 
@@ -7,8 +9,8 @@ public class OrderReceipt {
         this.order = order;
     }
 
-    private double TOTAL_SALES_TAX_PERCENT = 0d;
-    private double TOTAL_AMOUNT_PERCENT = 0d;
+    private double TOTAL_SALES_TAX_PERCENT = .10;
+    private double TOTAL_AMOUNT_PERCENT = 1.10;
     private final String SALES_TAX = "Sales Tax";
     private final String TOTAL_AMOUNT = "Total Amount";
 
@@ -20,16 +22,11 @@ public class OrderReceipt {
         printCustomerNameAndAddress(output);
         for (LineItem lineItem : order.getLineItems()) {
             printLineItems(output, lineItem);
-
-            double salesTax = lineItem.totalAmount() * .10;
-            TOTAL_SALES_TAX_PERCENT += salesTax;
-
-            TOTAL_AMOUNT_PERCENT += lineItem.totalAmount() + salesTax;
         }
 
-        printTotal(output, TOTAL_SALES_TAX_PERCENT, SALES_TAX);
+        printTotal(output, calculateTotal(order.getLineItems(), TOTAL_SALES_TAX_PERCENT), SALES_TAX);
 
-        printTotal(output, TOTAL_AMOUNT_PERCENT, TOTAL_AMOUNT);
+        printTotal(output, calculateTotal(order.getLineItems(), TOTAL_AMOUNT_PERCENT), TOTAL_AMOUNT);
 
         return output.toString();
     }
@@ -45,6 +42,14 @@ public class OrderReceipt {
 
     private void printHeader(StringBuilder output) {
         output.append("======Printing Orders======\n");
+    }
+
+    private double calculateTotal(List<LineItem> lineItems, double percent) {
+        double total = 0d;
+        for (int i = 0; i < lineItems.size(); i++) {
+            total += lineItems.get(i).totalAmount() * percent;
+        }
+        return total;
     }
 
     private void printLineItems(StringBuilder output, LineItem lineItem) {
